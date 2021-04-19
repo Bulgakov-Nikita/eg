58518e2cbf4f9268b2179581a4cdc172d96f3a16
XxxdimooonxxX <63523101+XxxdimooonxxX@users.noreply.github.com>
2021-04-19 13:41:02 +0300

    Rename field
    without comments

--- selects.sql
+++ selects.sql
@@ -1,22 +1,22 @@
 # первый селект
-SELECT st.FIO, ob.obj AS `Предмет`, gp.`group`, AVG(mk.mark) AS `Ср. балл`, mk.date_mark 
+SELECT st.FIO, ob.`name` AS `Предмет`, gp.`name`, AVG(mk.mark) AS `Ср. балл`, mk.date_mark 
 FROM students st JOIN marks mk ON st.id=mk.students_id
 JOIN `groups` gp ON st.groups_id=gp.id
 JOIN `objects` ob ON mk.objects_id=ob.id
-WHERE gp.`group`="КЭК" AND ob.obj="Математика"
+WHERE gp.`name`="КЭК" AND ob.`name`="Математика"
 AND mk.date_mark BETWEEN '2021-01-09' AND '2021-06-20' GROUP BY FIO;
 
 # второй селект
-SELECT st.FIO, ob.obj AS `Предмет`, gp.`group`, mk.mark, mk.date_mark 
+SELECT st.FIO, ob.`name` AS `Предмет`, gp.`name`, mk.mark, mk.date_mark 
 FROM students st JOIN marks mk ON st.id=mk.students_id
 JOIN `groups` gp ON st.groups_id=gp.id
 JOIN `objects` ob ON mk.objects_id=ob.id
-WHERE gp.`group`="КЭК" AND ob.obj="Математика"
+WHERE gp.`name`="КЭК" AND ob.`name`="Математика"
 AND mk.date_mark LIKE "2021%"
 ORDER BY mk.mark DESC;
 
 # третий селект
-SELECT teachers.teacher FROM teachers
+SELECT teachers.`name` FROM teachers
 JOIN lessons ON teachers.id=lessons.teachers_id
 WHERE name_lesson IS NULL 
 AND date_lesson BETWEEN "2021-01-09" AND "2021-06-10";
