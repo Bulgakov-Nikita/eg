0c4a45d4bd3fd6b2285dd64079f337e26d868c49
Bulgakov-Nikita <77019664+Bulgakov-Nikita@users.noreply.github.com>
2021-04-19 11:25:33 +0300

    Новый журнал

---
+++ new_journal.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE new_journal;
+USE new_joutnal;
+
+# таблица с группами
+CREATE TABLE `groups` (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    `group` VARCHAR(16)
+);
+
+CREATE TABLE objects (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    obj VARCHAR(24)
+);
+
+# таблица преподавателей
+CREATE TABLE teachers (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    teacher VARCHAR(64)
+);
+
+CREATE TABLE students (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    FIO VARCHAR(64),
+    groups_id INT,
+    god_postypleniya DATETIME,
+    FOREIGN KEY (groups_id) REFERENCES `groups`(id)
+);
+
+CREATE TABLE marks(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    students_id INT,
+    objects_id INT,
+    teachers_id INT,
+    date_mark DATETIME,
+    mark VARCHAR(4),
+    FOREIGN KEY (students_id) REFERENCES students(id),
+    FOREIGN KEY (objects_id) REFERENCES objects(id),
+    FOREIGN KEY (teachers_id) REFERENCES teachers(id)
+);
+
+CREATE TABLE lessons (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name_lesson TEXT,
+    objects_id INT,
+    homework TEXT,
+    teachers_id INT,
+    groups_id INT,
+    FOREIGN KEY (objects_id) REFERENCES `objects`(id),
+    FOREIGN KEY (teachers_id) REFERENCES teachers(id),
+    FOREIGN KEY (groups_id) REFERENCES `groups`(id)
+);
