960fcc1d317654cf77458d7ed550b3e131b1516e
Jensen1488 <82801427+Jensen1488@users.noreply.github.com>
2021-04-19 11:53:50 +0300

    Изменение строк
    Изменение строк с 112 по 149, изменил атрибуты.

--- Journal.sql
+++ Journal.sql
@@ -109,42 +109,44 @@ insert into marks values (5, 6, 4, '2021/06/10', '4');
 insert into marks values (6, 6, 4, '2021/06/08', '3'); 
 
 #Добавление данных в таблицу (Дмитрий Кошелев)
-INSERT INTO students(FIO, gruppa, god_postyplenia) VALUES ('В.Д. Безруков', 'КПСО', '2019/10/02');#id = 1
-INSERT INTO students(FIO, gruppa, god_postyplenia) VALUES ('К.А. Валерьев', 'КПСО', '2018/09/02');#id = 2
-INSERT INTO students(FIO, gruppa, god_postyplenia) VALUES ('К.Е. Юрьевич', 'КПИ', '2017/08/25');#id = 3
-INSERT INTO students(FIO, gruppa, god_postyplenia) VALUES ('У.К. Хорькова', 'КЭК', '2019/09/01');#id = 4
+#Максим Бубнов изменение с 112 по ...
+INSERT INTO students VALUES ('В.Д. Безруков', 3, '2019/10/02');#id = 1/
+INSERT INTO students VALUES ('К.А. Валерьев', 3, '2018/09/02');#id = 2
+INSERT INTO students VALUES ('К.Е. Юрьевич', '1', '2017/08/25');#id = 3
+INSERT INTO students VALUES ('У.К. Хорькова', 2, '2019/09/01');#id = 4
 
-INSERT INTO marks VALUES ('1', 'Математика', 'Попова', '2020/09/12', '4');
-INSERT INTO marks VALUES ('1', 'Литература', 'Зайцева', '2021/01/30', '3');
-INSERT INTO marks VALUES ('2', 'История', 'Краснослободцев', '2019/09/05', '4');
-INSERT INTO marks VALUES ('3', 'Математика', 'Осколенко', '2018/10/12', '5');
-INSERT INTO marks VALUES ('4', 'Экономика', 'Абрамов', '2020/09/12', '5');
+INSERT INTO marks VALUES ('1', 1 , 8 , '2020/09/12', '4');
+INSERT INTO marks VALUES ('1', 8 , 2, '2021/01/30', '3');
+INSERT INTO marks VALUES ('2', 6, 4 , '2019/09/05', '4');
+INSERT INTO marks VALUES ('3', 1 , 1 , '2018/10/12', '5');
+INSERT INTO marks VALUES ('4', 5 , 3, '2020/09/12', '5');
 
 # Заполнение таблицы (Булгаков Никита)
-INSERT INTO students(FIO, gruppa, god_postyplenia) 
-	VALUES ('Попов Анатолий Григорьевич', 'КЭК', '2020-08-19');
-INSERT INTO students(FIO, gruppa, god_postyplenia) 
-	 VALUES ('Татанов Борис Иванович', 'КЭК', '2020-08-28');
-INSERT INTO students(FIO, gruppa, god_postyplenia) 
-	VALUES ('Смирнов Павел Анатольевич', 'КПСО', '2020-08-24');
-INSERT INTO students(FIO, gruppa, god_postyplenia) 
-	VALUES ('Титов Григорий Павлович', 'КПИ', '2019-08-26');
-INSERT INTO students(FIO, gruppa, god_postyplenia) 
-	VALUES ('Остапов Анатолий Владимирович', 'КЭК', '2019-08-26');
-INSERT INTO students(FIO, gruppa, god_postyplenia) 
-	VALUES ('Павленко Иван Григорьевич', 'КПИ', '2019-08-07');
-INSERT INTO students(FIO, gruppa, god_postyplenia) 
-	VALUES ('Сидоров Анатолий Русланович', 'КПСО', '2019-08-16');
-INSERT INTO students(FIO, gruppa, god_postyplenia) 
-	VALUES ('Юсупов Никита Рудольфович', 'КПСО', '2019-08-15');
-INSERT INTO students(FIO, gruppa, god_postyplenia) 
-	VALUES ('Сомов Владимир Григорьевич', 'КЭК', '2019-08-18');
-INSERT INTO students(FIO, gruppa, god_postyplenia) 
-	VALUES ('Стоянов Павел Анатольевич', 'КПИ', '2019-08-21');
-INSERT INTO students(FIO, gruppa, god_postyplenia) 
-	VALUES ('Гриднев Егор Данилович', 'КЭК', '2019-08-22');
-INSERT INTO students(FIO, gruppa, god_postyplenia) 
-	VALUES ('Краснов Сергей Борисович', 'КПИ', '2019-08-20');
+INSERT INTO students 
+	VALUES ('Попов Анатолий Григорьевич', 2, '2020-08-19');
+INSERT INTO students
+	 VALUES ('Татанов Борис Иванович', 2, '2020-08-28');
+INSERT INTO students 
+	VALUES ('Смирнов Павел Анатольевич', 3, '2020-08-24');
+INSERT INTO students 
+	VALUES ('Титов Григорий Павлович', 1, '2019-08-26');
+INSERT INTO students
+	VALUES ('Остапов Анатолий Владимирович', 2, '2019-08-26');
+INSERT INTO students
+	VALUES ('Павленко Иван Григорьевич', 1, '2019-08-07');
+INSERT INTO students
+	VALUES ('Сидоров Анатолий Русланович', 3, '2019-08-16');
+INSERT INTO students
+	VALUES ('Юсупов Никита Рудольфович', 3, '2019-08-15');
+INSERT INTO students
+	VALUES ('Сомов Владимир Григорьевич', 2, '2019-08-18');
+INSERT INTO students
+	VALUES ('Стоянов Павел Анатольевич', 1, '2019-08-21');
+INSERT INTO students
+	VALUES ('Гриднев Егор Данилович', 2, '2019-08-22');
+INSERT INTO students
+	VALUES ('Краснов Сергей Борисович', 1, '2019-08-20');
+#END Максим
 	
 INSERT INTO marks(id_stud, object, teacher, data_mark, mark) 
 	VALUES (7, 'Математека', 'Оскаленко', '2021-04-10', '5');
