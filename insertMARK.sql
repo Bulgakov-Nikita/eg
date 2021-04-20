96ba1ce2570b5c328310a3bb51d874814e455bc1
XxxdimooonxxX <63523101+XxxdimooonxxX@users.noreply.github.com>
2021-04-20 10:02:07 +0300

    Необходимо переделать!!!!!!!

---
+++ insertMARK.sql
@@ -0,0 +1,104 @@
+#=-=-=-=-=-=-=-=-=-=-= 1 part =-=-=-=-=-=-=-=-=-=-=-=-
+insert into marks values (1, 1, 1, '2021/03/11', '4');
+insert into marks values (2, 1, 1, '2021/03/14', '4');
+insert into marks values (3, 1, 1, '2021/03/18', '5');
+insert into marks values (4, 1, 1, '2021/03/17', '5');
+insert into marks values (5, 1, 1, '2021/03/12', '5');
+insert into marks values (6, 1, 1, '2021/03/12', '3');
+insert into marks values (1, 2, 2, '2021/03/15', '3');
+insert into marks values (2, 2, 2, '2021/03/14', '4');
+insert into marks values (3, 2, 2, '2021/03/13', '4');
+insert into marks values (4, 2, 2, '2021/03/11', '5');
+insert into marks values (5, 2, 2, '2021/03/18', '4');
+insert into marks values (6, 2, 2, '2021/03/11', '4');
+insert into marks values (1, 8, 2, '2021/03/12', '5');
+insert into marks values (2, 8, 2, '2021/03/17', '5');
+insert into marks values (3, 8, 2, '2021/03/14', '5');
+insert into marks values (4, 8, 2, '2021/03/13', '4');
+insert into marks values (5, 8, 2, '2021/03/11', '4');
+insert into marks values (6, 8, 2, '2021/03/16', '5');
+insert into marks values (1, 3, 7, '2021/03/12', '4');
+insert into marks values (2, 3, 7, '2021/03/14', '4');
+
+#=-=-=-=-=-=-=-=-=-=-= 2 part =-=-=-=-=-=-=-=-=-=-=-=-
+insert into marks values (3, 3, 7, '2021/03/17', '4');
+insert into marks values (4, 3, 7, '2021/03/17', '4');
+insert into marks values (5, 3, 7, '2021/03/18', '5');
+insert into marks values (6, 3, 7, '2021/03/11', '4');
+insert into marks values (1, 4, 8, '2021/03/18', '5');
+insert into marks values (2, 4, 7, '2021/03/14', '5');
+insert into marks values (3, 4, 7, '2021/03/16', '5');
+insert into marks values (4, 4, 7, '2021/03/15', '4');
+insert into marks values (5, 4, 7, '2021/03/15', '3');
+insert into marks values (6, 4, 7, '2021/03/15', '4');
+insert into marks values (1, 6, 6, '2021/03/15', '4');
+insert into marks values (2, 6, 6, '2021/03/17', '4');
+insert into marks values (3, 6, 6, '2021/03/14', '4');
+insert into marks values (4, 6, 6, '2021/03/18', '5');
+insert into marks values (5, 6, 6, '2021/03/16', '5');
+insert into marks values (6, 6, 6, '2021/03/13', '4');
+insert into marks values (1, 1, 1, '2021/06/09', '5');
+insert into marks values (2, 1, 1, '2021/06/09', '4');
+insert into marks values (3, 1, 1, '2021/06/10', '4');
+insert into marks values (4, 1, 1, '2021/06/07', '5');
+insert into marks values (5, 1, 1, '2021/06/05', '3');
+
+#=-=-=-=-=-=-=-=-=-=-= 3 part =-=-=-=-=-=-=-=-=-=-=-=-
+insert into marks values (6, 1, 1, '2021/06/12', '5');
+insert into marks values (1, 2, 2, '2021/06/09', '5');
+insert into marks values (2, 2, 2, '2021/06/07', '3');
+insert into marks values (3, 2, 2, '2021/06/11', '5');
+insert into marks values (4, 2, 2, '2021/06/08', '5');
+insert into marks values (5, 2, 2, '2021/06/12', '4');
+insert into marks values (6, 2, 2, '2021/06/13', '4');
+insert into marks values (1, 8, 3, '2021/06/09', '4');
+insert into marks values (2, 8, 3, '2021/06/09', '5');
+insert into marks values (3, 8, 3, '2021/06/11', '3');
+insert into marks values (4, 8, 3, '2021/06/07', '3');
+insert into marks values (5, 8, 3, '2021/06/08', '5');
+insert into marks values (6, 8, 3, '2021/06/12', '4');
+insert into marks values (1, 2, 7, '2021/06/09', '5');
+insert into marks values (2, 2, 7, '2021/06/12', '5');
+insert into marks values (3, 2, 7, '2021/06/07', '5');
+insert into marks values (4, 2, 7, '2021/06/13', '4');
+insert into marks values (5, 2, 7, '2021/06/09', '5');
+insert into marks values (6, 2, 7, '2021/06/17', '4');
+insert into marks values (1, 4, 8, '2021/06/17', '4');
+insert into marks values (2, 4, 8, '2021/06/11', '5');
+
+#=-=-=-=-=-=-=-=-=-=-= 4 part =-=-=-=-=-=-=-=-=-=-=-=-
+insert into marks values (3, 4, 8, '2021/06/11', '3');
+insert into marks values (4, 4, 8, '2021/06/14', '5');
+insert into marks values (5, 4, 8, '2021/06/15', '5');
+insert into marks values (6, 4, 8, '2021/06/06', '4');
+insert into marks values (1, 6, 4, '2021/06/19', '4');
+insert into marks values (2, 6, 4, '2021/06/21', '5');
+insert into marks values (3, 6, 4, '2021/06/13', '5');
+insert into marks values (4, 6, 4, '2021/06/14', '5');
+insert into marks values (5, 6, 4, '2021/06/10', '4');
+insert into marks values (6, 6, 4, '2021/06/08', '3'); 
+INSERT INTO marks VALUES (1, 1, 8, '2020/09/12', '4');
+INSERT INTO marks VALUES (1, 8, 2, '2021/01/30', '3');
+INSERT INTO marks VALUES (2, 6, 4, '2019/09/05', '4');
+INSERT INTO marks VALUES (3, 1, 1, '2018/10/12', '5');
+INSERT INTO marks VALUES (4, 5, 3, '2020/09/12', '5');
+
+#=-=-=-=-=-=-=-=-=-=-=-=- ДЛЯ ЖЕЛАЮЩИХ =-=-=-=-=-=-=-=-=-=-=-=
+INSERT INTO marks(id_stud, object, teacher, data_mark, mark) 
+	VALUES (7, 'Математека', 'Оскаленко', '2021-04-10', '5');
+INSERT INTO marks(id_stud, object, teacher, data_mark, mark) 
+	VALUES (1, 'Русский язык', 'Зайцева', '2021-04-10', '4');
+INSERT INTO marks(id_stud, object, teacher, data_mark, mark) 
+	VALUES (8, 'Математека', 'Оскаленко', '2021-04-10', '5');
+INSERT INTO marks(id_stud, object, teacher, data_mark, mark) 
+	VALUES (3, 'Литература', 'Зайцева', '2021-04-10', '3');
+INSERT INTO marks(id_stud, object, teacher, data_mark, mark) 
+	VALUES (4, 'Математека', 'Оскаленко', '2021-04-10', '5');
+INSERT INTO marks(id_stud, object, teacher, data_mark, mark) 
+	VALUES (8, 'Математека', 'Оскаленко', '2021-04-11', '4');
+INSERT INTO marks(id_stud, object, teacher, data_mark, mark) 
+	VALUES (2, 'Информатика', 'Попова', '2021-04-10', '3');
+INSERT INTO marks(id_stud, object, teacher, data_mark, mark) 
+	VALUES (9, 'Информатика', 'Попова', '2021-04-10', 'Н');
+INSERT INTO marks(id_stud, object, teacher, data_mark, mark) 
+	VALUES (5, 'Английский', 'Провоторова', '2021-04-10', '4');
