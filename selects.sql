0a29d0bc87b21901069f5495f278223163b810aa
loliDeanon <82580177+loliDeanon@users.noreply.github.com>
2021-04-20 13:42:54 +0300

    Изменил первый запрос

--- selects.sql
+++ selects.sql
@@ -1,10 +1,11 @@
 # первый селект
-SELECT st.FIO, ob.`name` AS `Предмет`, gp.`name`, AVG(mk.mark) AS `Ср. балл`, mk.date_mark 
+SELECT st.FIO, ob.`name` AS `Предмет`, gp.`name`, AVG(mk.mark) AS `Ср. балл`, ls.date_lesson
 FROM students st JOIN marks mk ON st.id=mk.students_id
+join lessons ls on mk.lessons_id=ls.id
 JOIN `groups` gp ON st.groups_id=gp.id
-JOIN `objects` ob ON mk.objects_id=ob.id
-WHERE gp.`name`="КЭК" AND ob.`name`="Математика"
-AND mk.date_mark BETWEEN '2021-01-09' AND '2021-06-20' GROUP BY FIO;
+JOIN `objects` ob ON ls.objects_id=ob.id
+WHERE gp.`name`="КЭК-2020" AND ob.`name`="Математика"
+AND ls.date_lesson BETWEEN '2021-01-09' AND '2021-06-20' GROUP BY FIO;
 
 # второй селект
 SELECT st.FIO, ob.`name` AS `Предмет`, gp.`name`, mk.mark, mk.date_mark 
