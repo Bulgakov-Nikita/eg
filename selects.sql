ced73c28cf9427e7500828130069dcbd956a3bc1
Bulgakov-Nikita <77019664+Bulgakov-Nikita@users.noreply.github.com>
2021-04-20 13:44:05 +0300

    добавил четвёртый селект

--- selects.sql
+++ selects.sql
@@ -21,3 +21,11 @@ SELECT teachers.`name` FROM teachers
 JOIN lessons ON teachers.id=lessons.teachers_id
 WHERE name_lesson IS NULL 
 AND date_lesson BETWEEN "2021-01-09" AND "2021-06-10";
+
+# четвёртый запрос
+SELECT ob.`name`, gp.`name`, COUNT(ob.id) AS `Кол-во пар` FROM teachers
+JOIN lessons ON teachers.id=lessons.teachers_id
+JOIN objects ob ON ob.id=lessons.objects_id
+JOIN `groups` gp ON gp.id=lessons.groups_id
+AND date_lesson BETWEEN "2021-01-09" AND "2021-06-10"
+GROUP BY ob.id, gp.id;
